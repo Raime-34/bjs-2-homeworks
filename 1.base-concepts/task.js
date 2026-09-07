081886ff1ef4fd711042774dd4109870e6590e96
Raime-34 <hushnud2000@gmail.com>
2026-09-07 17:31:43 +0300

    основы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+
+  let discriminant = b**2-4*a*c;
+  switch (true) {
+    case discriminant == 0:
+        arr.push(-b/(2*a));
+        break;
+    case discriminant > 0:
+        arr.push((-b+Math.sqrt(discriminant))/(2*a));
+        arr.push((-b-Math.sqrt(discriminant))/(2*a));
+        break;
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentNormalized = percent/1200;
+  let body = amount - contribution;
+  let fee = body * (percentNormalized + (percentNormalized / (((1 + percentNormalized) ** countMonths) - 1)));
+  return Number((fee * countMonths).toFixed(2));
 }
